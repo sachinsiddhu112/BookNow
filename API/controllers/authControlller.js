2a0d7608ff7e47a461b73ae26840f9ad678e1c9b
Sachin <sachinsiddhu112@gmail.com>
2024-06-15 18:42:15 +0530

    deploying on render1

--- API/controllers/authControlller.js
+++ API/controllers/authControlller.js
@@ -39,7 +39,7 @@ export const login = async (req, res, next) => {
         const { password, isAdmin, ...otherDetails } = user._doc;
 
         res.cookie("access_token",token,{
-            httpOnly:true,
+            httpsOnly:true,
 
         }).status(200).json({ details:{...otherDetails},isAdmin });
     }
