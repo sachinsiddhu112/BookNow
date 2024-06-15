ed1da1cc5a1c6b64dec8cb1d562e19fc69692423
Sachin <sachinsiddhu112@gmail.com>
2024-06-15 16:03:27 +0530

    changes1

--- API/controllers/authControlller.js
+++ API/controllers/authControlller.js
@@ -32,7 +32,7 @@ export const login = async (req, res, next) => {
 
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
 
-        if (!isPasswordCorrect) return next(createError(400, "wrong password or username"));
+        if (!isPasswordCorrect) return next(createError(400, "wrong password/ username"));
 
         const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin },process.env.JWT);
 
